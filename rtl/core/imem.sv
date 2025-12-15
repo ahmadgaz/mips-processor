8637f0ff09aa5ce094495e64882865e33aab7046
ahmadgaz <gazali.contact@gmail.com>
2025-12-14 23:00:45 -0800

    undid parametrized memfile

--- rtl/core/imem.sv
+++ rtl/core/imem.sv
@@ -3,13 +3,9 @@ module imem (
     output wire [31:0] y
 );
   reg [31:0] rom[64];
-  string memfile;
 
   initial begin
-    if (!$value$plusargs("MEMFILE=%s", memfile)) begin
-      $fatal(1, "[IMEM] Must include +MEMFILE argument");
-    end
-    $readmemh(memfile, rom);
+    $readmemh("memfile.dat", rom);
   end
 
   assign y = rom[a];
