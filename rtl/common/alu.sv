a2200b5ea2833c155e9669946ca6835afdb3c90e
ahmadgaz <gazali.contact@gmail.com>
2025-12-18 09:09:12 -0800

    fixed slt to be signed

--- rtl/common/alu.sv
+++ rtl/common/alu.sv
@@ -16,7 +16,7 @@ module alu #(
       3'b110:  y = a - b;  // SUB
       3'b000:  y = a & b;  // AND
       3'b001:  y = a | b;  // OR
-      3'b111:  y = (a < b) ? 1 : 0;  // SLT
+      3'b111:  y = ($signed(a) < $signed(b)) ? 32'd1 : 32'd0;  // SLT
       3'b100:  y = b << shamt;  // SLL
       3'b101:  y = b >> shamt;  // SRL
       default: y = 'x;
