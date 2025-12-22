12270781435b9647b509ca3bfdd9c3d41d4802e6
ahmadgaz <gazali.contact@gmail.com>
2025-12-21 18:03:28 -0800

    changed wire regfile outputs to logic

--- rtl/core/regfile.sv
+++ rtl/core/regfile.sv
@@ -1,15 +1,15 @@
 module regfile (
     input  wire        clk,
+    input  wire        rst,
     input  wire        we,
     input  wire [ 4:0] ra1,
     input  wire [ 4:0] ra2,
     input  wire [ 4:0] ra3,
     input  wire [ 4:0] wa,
     input  wire [31:0] wd,
-    output wire [31:0] rd1,
-    output wire [31:0] rd2,
-    output wire [31:0] rd3,
-    input  wire        rst
+    output logic [31:0] rd1,
+    output logic [31:0] rd2,
+    output logic [31:0] rd3
 );
   reg [31:0] rf[32];
   integer i;
